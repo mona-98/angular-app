35730aead02bf9ceaa70fbb77cd5e2492167927e
Mohana20 <mohana.b@entosstech.com>
2022-05-23 22:19:11 +0530

    changes for clock

--- src/app/components/clock/clock.component.ts
+++ src/app/components/clock/clock.component.ts
@@ -1,9 +1,6 @@
 import { Component, ViewChild, ElementRef, Input, OnInit, AfterViewInit, OnDestroy } from '@angular/core';  
-import { ChangeDetectionStrategy, NgZone } from '@angular/core';  
-import { Observable, timer, Subscription } from 'rxjs';  
-import { timeInterval, map } from 'rxjs/operators';  
-import { formatDate } from '@angular/common';  
-import { DatePipe } from '@angular/common';
+import {getAllCountries,getTimezonesForCountry} from 'countries-and-timezones';
+
 
 @Component({
   selector: 'app-clock',
@@ -12,19 +9,52 @@ import { DatePipe } from '@angular/common';
 })
 export class ClockComponent implements OnInit {
   time;
+  countries=[];
+  selectedCountryId=" ";
+  selectedTimezone=" ";
+  timezones=[];
   constructor(){
 
   }
   ngOnInit() {
     const self = this;
-    setInterval(function () { self.time = self.getCurrentTime();}, 1000);
-   
+    
+    let allCountries = getAllCountries()
+    Object.keys(allCountries).forEach(function(key) {
+      self.countries.push(allCountries[key])
+    })
   }
-  getCurrentTime() {
-    let currentDate = new Date();
-    let time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+  getCurrentTime(currentDate) {
+    //let currentDate = new Date();
+    console.log("getcurrenttime method is called");
+    //let time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+     let time =currentDate.toLocaleTimeString();
     return time
   }
- 
+  click(){
+    alert("click function is called");
+    console.log(getAllCountries());
+  }
+
+  onSelectCountry() {
+      const self = this;
+      this.selectedTimezone=" ";
+      this.time=" ";
+      this.timezones=[];
+      console.log("selectedCountryId",this.selectedCountryId);
+      let timezone=getTimezonesForCountry(this.selectedCountryId);
+      Object.keys(timezone).forEach(function(key) {
+        self.timezones.push(timezone[key])
+      })
+  }
+  
+  onSelectTimezone() {
+         console.log("selectedTimezone",this.selectedTimezone);
+         const self = this;
+        setInterval(function () { 
+          let date = new Date(new Date().toLocaleString('en-US', {timeZone: self.selectedTimezone}))
+          self.time = self.getCurrentTime(date);}, 1000);
+        
+  }
 }
 
